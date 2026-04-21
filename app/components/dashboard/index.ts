0f42adea65df470eed1de6587b09de384f4da3fc
Zain Khan <zain.khan23@st.niituniversity.in>
2026-04-22 04:47:46 +0530

    extra files

--- app/components/dashboard/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as DashboardLayout } from "./DashboardLayout";
-export { default as MetricCard } from "./MetricCard";
-export { default as RiskTable } from "./RiskTable";
-export { default as ComplianceStatus } from "./ComplianceStatus";
-export { default as DataFlowVisualization } from "./DataFlowVisualization";
