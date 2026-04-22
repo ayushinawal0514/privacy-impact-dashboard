35b1678dbb894db9f89fcaa685736644b4b42c8c
Zain Khan <zain.khan23@st.niituniversity.in>
2026-04-22 17:04:19 +0530

    Remove tracked Next.js build cache files

--- frontend/.next/dev/server/chunks/ssr/c6515_frontend__next-internal_server_app_dashboard_admin_page_actions_0109dc88.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Project/capstone_project/frontend/.next-internal/server/app/dashboard/admin/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=c6515_frontend__next-internal_server_app_dashboard_admin_page_actions_0109dc88.js.map
